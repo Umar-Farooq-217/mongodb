5a763b0d04b7022aca804f1a9340209f8ac4b441
Your Name <uf29664@gmail.com>
2024-01-27 21:13:06 +0500

    mongo

--- app/components/studentsButton/StudentButton.js
+++ app/components/studentsButton/StudentButton.js
@@ -4,6 +4,14 @@ export default function StudentButton() {
     const [data,setData]=useState([])
   return (
     <div>
+    <h1>Student Detail</h1>
+    {
+      
+    }
+
+
+
+
     <button onClick={getHandler}>See Students</button>
     
     </div>
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,9 +1,22 @@
+import { studentModel } from '@/model/students'
 import React from 'react'
+import StudentButton from '../components/studentsButton/StudentButton';
+
+const getHandler=async()=>{
+  const data =await studentModel.find();
+  console.log('data',data);
+  return data
+
+}
+
+
+
 
 export default function page() {
+
   return (
     <div>
-
+<StudentButton submit={getHandler}/>
     
     
     
